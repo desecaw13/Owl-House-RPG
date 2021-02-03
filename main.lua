905e51b9c6157ac9c5810b0d3ece5ba995a810dc
Joe-O <47424966+Joe-O@users.noreply.github.com>
2021-02-02 20:47:35 -0700

    finished core movement
    moving forward with other parts now.
    speed still not good.

--- main.lua
+++ main.lua
@@ -10,33 +10,35 @@ function love.load()
 end
 
 function love.draw()
-	love.graphics.draw(player.image,player.trans)
+	--love.graphics.print()
 	-- make a table of all objects that should be rendered and go through it drawing them
-	love.graphics.print("player: "..player.body:getX().." "..player.body:getY())
-	love.graphics.print("epoint: "..x.." "..y,0,10)
-	love.graphics.print("mouse: "..love.mouse.getX().." "..love.mouse.getY(),0,20)
-	love.graphics.print("vector: "..vx.." "..vy.."\n"..done,0,30)
+	love.graphics.draw(player.image,player.trans)
 end
 
-done = 0
+mx,my = 0,0
 function love.update(dt)
-	if love.mouse.isDown(1)then
+	if love.mouse.isDown(1) then
 		mx,my = love.mouse.getPosition()
-		x,y= move(player,mx,my)--x,y output is for debuging only, remove soon
 	end
+	move(player,mx,my)
 	-- calculate route (use love.physics for obstacles) between player starting point and ending point
 end
 
-vx,vy,x,y=0,0,0,0
 function move(obj,x,y)
-	if (x~=obj.body:getX() and y~=obj.body:getY()) or (wx~=0 and wy ~= 0) then
-		vx = x/2 - obj.body:getX()/2
-		vy = y/2 - obj.body:getY()/2
+	if not (compare(x,obj.body:getX()) and compare(y,obj.body:getY())) then
+		run = x - obj.body:getX()
+		rise = y - obj.body:getY()
+		mag = math.sqrt((rise^2) + (run^2))
+		vx = run / mag
+		vy = rise / mag
+		
 		obj.body:setX(obj.body:getX() + vx)
 		obj.body:setY(obj.body:getY() + vy)
-		obj.trans = love.math.newTransform(obj.body:getX() - obj.image:getWidth() / 2, obj.body:getY() - obj.image:getHeight() / 2)
-	else
-		done = 1--why if no work? try repeat?
+	
+		obj.trans = love.math.newTransform(obj.body:getX() - obj.image:getWidth()/2, obj.body:getY() - obj.image:getHeight()/2)
 	end
-	return x,y
+end
+
+function compare(n1,n2)
+	return (n1 < n2 + 0.5) and (n1 > n2 - 0.5)
 end
